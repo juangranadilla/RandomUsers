96243ac993fbddcd942c9a735f0f60d8c1560e64
juangranadilla <juanillogm91@gmail.com>
2019-05-07 14:00:24 +0200

    Adding seed to api call

--- app/src/main/java/com/juangm/randomusers/data/repository/UsersService.kt
+++ app/src/main/java/com/juangm/randomusers/data/repository/UsersService.kt
@@ -6,8 +6,14 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+private const val DEFAULT_SEED = 8173
+
 interface UsersService {
 
     @GET(" ")
-    fun getRandomUsers(@Query("page") page: Int, @Query("results") results: Int): Single<ResponseDto>
+    fun getRandomUsers(
+        @Query("page") page: Int,
+        @Query("results") results: Int,
+        @Query("seed") seed: Int = DEFAULT_SEED
+    ): Single<ResponseDto>
 }
